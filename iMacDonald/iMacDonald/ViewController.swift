41d18c17c32196a523dc82f24ba594f3a2e13966
taeryu7 <taeryu97@gmail.com>
2024-11-25 18:02:25 +0900

    초기세팅(설정, 이미지, 컬러값, 파일)

---
+++ iMacDonald/iMacDonald/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iMacDonald
+//
+//  Created by 유태호 on 11/25/24.
+//
+
+import UIKit
+
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
