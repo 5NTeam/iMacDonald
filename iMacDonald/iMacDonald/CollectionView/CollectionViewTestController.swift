056295f4dbf637fab44b0e8b73188e0a5c267a1a
Crois0509 <crois0509@naver.com>
2024-11-26 11:20:34 +0900

    ✨[Feat] 컬렉션뷰 테스트 파일 생성

---
+++ iMacDonald/iMacDonald/CollectionView/CollectionViewTestController.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewTestController.swift
+//  iMacDonald
+//
+//  Created by 장상경 on 11/26/24.
+//
+
+import UIKit
+
+class CollectionViewTestController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
