90999ad592a60acba3ad457cbcc1aec8f9274164
sonny <gyeomsony@gmail.com>
2024-11-26 13:18:48 +0900

    ✨ 테이블뷰 DataSource 수정 및 액션 설정, tableViewCell 초기화 구문 추가

--- iMacDonald/iMacDonald/TableView/MenuTableViewCell.swift
+++ iMacDonald/iMacDonald/TableView/MenuTableViewCell.swift
@@ -19,4 +19,18 @@ class MenuTableViewCell: UITableViewCell {
     let increaseButton = UIButton() // 수량 증가 버튼
     let deleteButton = UIButton() // 삭제 버튼
 
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI() // 셀의 UI를 설정하는 커스텀 메서드
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        // 메뉴 이미지 뷰
+        
+    }
+
 }
--- iMacDonald/iMacDonald/TableView/TableView.swift
+++ iMacDonald/iMacDonald/TableView/TableView.swift
@@ -32,8 +32,7 @@ class MenuDataViewController: UIViewController, UITableViewDelegate, UITableView
         MenuData(name: "클래식버거", price: 16000, image: UIImage(named: "classicburger"), category: .burger),
         MenuData(name: "더블버거", price: 16000, image: UIImage(named: "doubleburger"), category: .burger)
     ]
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -45,25 +44,46 @@ class MenuDataViewController: UIViewController, UITableViewDelegate, UITableView
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MenuCell")
         tableView.rowHeight = 80
         
-        // 메뉴 항목에 맞춰 높이 수정
+        // 메뉴 항목에 맞춰 높이 자동 수정
         let tableViewHeight = CGFloat(dummyBurgers.count) * tableView.rowHeight
         
         // SnapKit으로 레이아웃 설정
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.centerY).offset(40) // 화면 절반보다 좀 더 아래로
-            make.leading.equalToSuperview().offset(16)
-            make.trailing.equalToSuperview().offset(-16)
-            make.bottom.equalTo(tableViewHeight) // 안전영역에 맞게
+            make.edges.equalToSuperview()
         }
     }
-// 테이블 뷰 갯수마다 자동으로 섹션 생성
+    // 테이블 뷰 갯수마다 자동으로 섹션 생성
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dummyBurgers.count
     }
-
+    // 테이블 뷰 DataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
-        let burger = dummyBurgers[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as? MenuTableViewCell else {
+            return UITableViewCell()
+        }
+        // 데이터 설정
+        let item = dummyBurgers[indexPath.row]
+        cell.nameLabel.text = item.name
+        cell.priceLabel.text = "\(item.price)원"
+        cell.quantityLabel.text = "\(item.category)"
+        
+        // 버튼 액션 설정
+        cell.decreaseButton.addTarget(self, action: #selector(decreaseQuantity), for: .touchUpInside)
+        cell.increaseButton.addTarget(self, action: #selector(increaseQuantity), for: .touchUpInside)
+        cell.deleteButton.addTarget(self, action: #selector(deleteItem), for: .touchUpInside)
+        
         return cell
     }
+    @objc func decreaseQuantity(sender: UIButton) {
+        // 수량 감소 로직
+    }
+    
+    @objc func increaseQuantity(sender: UIButton) {
+        // 수량 증가 로직
+    }
+    
+    @objc func deleteItem(sender: UIButton) {
+        // 아이템 삭제 로직
+    }
 }
+
