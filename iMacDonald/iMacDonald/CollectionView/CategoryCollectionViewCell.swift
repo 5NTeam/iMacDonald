911ef0baaf64d3e0b620bbc78777af0c8b2ee85e
Crois0509 <crois0509@naver.com>
2024-11-26 14:41:35 +0900

    ♻️[Refactor] 주석 설명 추가

--- iMacDonald/iMacDonald/CollectionView/CategoryCollectionViewCell.swift
+++ iMacDonald/iMacDonald/CollectionView/CategoryCollectionViewCell.swift
@@ -9,8 +9,10 @@ import UIKit
 import SnapKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
+    // 컬렉션뷰에 추가할 때 입력할 매개변수(identifier) 정의
     static let identifier: String = "CategoryCollectionViewCell"
     
+    // 카테고리의 레이블 값 정의
     private let categoryLabel: UILabel = {
         let label = UILabel()
         label.text = "Vegan"
@@ -25,6 +27,9 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
+    
+    /// 컬렉션뷰 셀 아이템 기본 설정
+    /// - Parameter frame: 컬렉션뷰 셀 아이템의 frame 값 = .zero 설정
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = UIColor.clear
@@ -41,10 +46,17 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    
+    /// 필수 생성 지정자
+    /// - Parameter coder: 인터페이스 빌더 엔코딩 값
+    ///
+    /// 인터페이스 빌더로 현재 뷰를 불러올 경우 fatalError를 발생시키지 않고 뷰 인스턴스를 생성
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
+    /// 선택된 카테고리를 강조하는 메소드
+    /// - Parameter isSelected: 현재 카테고리가 선택되었는지 확인
     func selectCategory(_ isSelected: Bool) {
         guard isSelected else { return }
         self.categoryLabel.backgroundColor = UIColor.personal
