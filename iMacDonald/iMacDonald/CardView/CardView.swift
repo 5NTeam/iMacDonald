09f1c5743916bcce3920742428f42bea40e9f614
HwangSeokBeom <tjrqja07@naver.com>
2024-11-26 21:08:42 +0900

    ♻️ refactor : 이미뷰 설정, 레이블 2개 설정, 버튼 설정

--- iMacDonald/iMacDonald/CardView/CardView.swift
+++ iMacDonald/iMacDonald/CardView/CardView.swift
@@ -10,6 +10,7 @@ import SnapKit
 
 class CardView: UIView {
     
+    // CardView의 구성요소 이미지, 레이블 2개, 버튼 1개 객체 생성
     private let imageView = UIImageView()
     private let titleLabel = UILabel()
     private let priceLabel = UILabel()
@@ -34,6 +35,32 @@ class CardView: UIView {
         self.layer.shadowOffset = CGSize(width: 0, height: 2)
         self.layer.shadowRadius = 4
         self.backgroundColor = .white
+        
+        // 이미지뷰 설정
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        addSubview(imageView)
+        
+        // 메뉴이름 레이블 설정
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        titleLabel.textColor = .black
+        titleLabel.numberOfLines = 0 // 여러 줄로 표시 가능
+        titleLabel.lineBreakMode = .byWordWrapping // 단어 단위로 줄바꿈
+        addSubview(titleLabel)
+        
+        // 가격 레이블 설정
+        priceLabel.font = UIFont.systemFont(ofSize: 14)
+        priceLabel.textColor = .darkGray
+        addSubview(priceLabel)
+        
+        // 버튼 설정
+        let buttonImage = UIImage(systemName: "plus.circle.fill")
+        button.setImage(buttonImage, for: .normal)
+        button.tintColor = UIColor(named: "PersonalColor")
+        //button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        addSubview(button)
+        
     }
+    
     
 }
