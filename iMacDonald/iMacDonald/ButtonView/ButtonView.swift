8a991e05b584016c69e497534448698f700acbd0
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-11-26 20:55:34 +0900

    📝 docs : 문서 수정

--- iMacDonald/iMacDonald/ButtonView/ButtonView.swift
+++ iMacDonald/iMacDonald/ButtonView/ButtonView.swift
@@ -1,11 +1,11 @@
-//
-//  ButtonView.swift
-//  iMacDonald
-//
-//  Created by 유태호 on 11/25/24.
-//
+/**
+* 작성자: 양정무
+* 파일명: ButtonView.swift
+* 기능: 결제 정보 표시 및 결제/취소 버튼을 포함하는 하단 뷰
+*/
 
 import UIKit
+
 import SnapKit
 
 class CheckoutView: UIView {
@@ -41,7 +41,9 @@ class CheckoutView: UIView {
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.red.cgColor
         button.layer.cornerRadius = 25
-        button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        button.addTarget(self,
+                action: #selector(cancelButtonTapped),
+                for: .touchUpInside)
         return button
     }()
     
@@ -51,7 +53,9 @@ class CheckoutView: UIView {
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .red
         button.layer.cornerRadius = 25
-        button.addTarget(self, action: #selector(paymentButtonTapped), for: .touchUpInside)
+        button.addTarget(self,
+                action: #selector(paymentButtonTapped),
+                for: .touchUpInside)
         return button
     }()
     
@@ -62,8 +66,8 @@ class CheckoutView: UIView {
         stackView.distribution = .fillEqually
         return stackView
     }()
-
-    // MARK: - Init
+    
+    // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -73,11 +77,21 @@ class CheckoutView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Setup
+    // MARK: - UI Setup
+    /**
+    * 기능: UI 컴포넌트들을 초기화하고 제약조건을 설정하는 메서드
+    */
     private func setupUI() {
         backgroundColor = .white
         
-        // Add subviews
+        configureSubviews()
+        setupConstraints()
+    }
+    
+    /**
+    * 기능: 서브뷰들을 추가하고 구성하는 메서드
+    */
+    private func configureSubviews() {
         addSubview(infoStackView)
         infoStackView.addArrangedSubview(totalQuantityLabel)
         infoStackView.addArrangedSubview(totalAmountLabel)
@@ -85,8 +99,12 @@ class CheckoutView: UIView {
         addSubview(buttonStackView)
         buttonStackView.addArrangedSubview(cancelButton)
         buttonStackView.addArrangedSubview(paymentButton)
-        
-        // Setup constraints
+    }
+    
+    /**
+    * 기능: Auto Layout 제약조건을 설정하는 메서드
+    */
+    private func setupConstraints() {
         infoStackView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.left.equalToSuperview().offset(16)
@@ -103,19 +121,33 @@ class CheckoutView: UIView {
     }
     
     // MARK: - Actions
+    /**
+    * 기능: 취소 버튼 탭 이벤트 처리
+    */
     @objc private func cancelButtonTapped() {
         // 취소 버튼 동작 구현
     }
     
+    /**
+    * 기능: 결제 버튼 탭 이벤트 처리
+    */
     @objc private func paymentButtonTapped() {
         // 결제 버튼 동작 구현
     }
     
     // MARK: - Public Methods
+    /**
+    * 기능: 총 수량을 업데이트하는 메서드
+    * @param quantity: 표시할 총 수량
+    */
     func updateTotalQuantity(_ quantity: Int) {
         totalQuantityLabel.text = "총 \(quantity)개"
     }
     
+    /**
+    * 기능: 총 금액을 업데이트하는 메서드
+    * @param amount: 표시할 총 금액
+    */
     func updateTotalAmount(_ amount: Int) {
         totalAmountLabel.text = "\(amount.formattedWithSeparator)원"
     }
@@ -123,6 +155,10 @@ class CheckoutView: UIView {
 
 // MARK: - Extensions
 extension Int {
+    /**
+    * 기능: 숫자를 천 단위 구분자가 포함된 문자열로 변환
+    * @return: 천 단위 구분자가 포함된 문자열
+    */
     var formattedWithSeparator: String {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
