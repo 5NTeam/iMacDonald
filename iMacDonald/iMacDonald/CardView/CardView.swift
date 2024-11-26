c541eabdcec33e3932500a7a305d535ab8897134
HwangSeokBeom <tjrqja07@naver.com>
2024-11-26 22:34:23 +0900

    ✅ test : 레이블 2개와 버튼 제약 조건 수정

--- iMacDonald/iMacDonald/CardView/CardView.swift
+++ iMacDonald/iMacDonald/CardView/CardView.swift
@@ -12,14 +12,15 @@ class CardView: UIView {
     
     // CardView의 구성요소 이미지, 레이블 2개, 버튼 1개 객체 생성
     private let imageView = UIImageView()
-    private let titleLabel = UILabel()
+    private let nameLabel = UILabel()
     private let priceLabel = UILabel()
     private let button = UIButton()
     
-    // 메뉴 이름, 가격, 이미지, 카테고리를 가지는 객체 생성하고 초기화
-    init(title: String, price: Int, image: UIImage?, category: Categorys) {
+    // 메뉴 이름, 가격, 이미지를 가지는 객체 생성하고 초기화
+    init(name: String, price: Int, image: UIImage?) {
         super.init(frame: .zero)
         setupView() // 뷰
+        configure(name: name, price: price, image: image) // 데이터 바인딩 호출
     }
     
     required init?(coder: NSCoder) {
@@ -29,6 +30,7 @@ class CardView: UIView {
     // 카드 뷰 UI 설정
     private func setupView() {
         // 카드뷰 스타일 설정
+        self.clipsToBounds = true
         self.layer.cornerRadius = 12
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOpacity = 0.2
@@ -39,22 +41,24 @@ class CardView: UIView {
         // 이미지뷰 설정
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
+        imageView.backgroundColor = .yellow
         addSubview(imageView)
         
         // 메뉴이름 레이블 설정
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        titleLabel.textColor = .black
-        titleLabel.numberOfLines = 0 // 여러 줄로 표시 가능
-        titleLabel.lineBreakMode = .byWordWrapping // 단어 단위로 줄바꿈
-        addSubview(titleLabel)
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        nameLabel.textColor = .black
+        nameLabel.numberOfLines = 0 // 여러 줄로 표시 가능
+        nameLabel.lineBreakMode = .byWordWrapping // 단어 단위로 줄바꿈
+        addSubview(nameLabel)
         
         // 가격 레이블 설정
-        priceLabel.font = UIFont.systemFont(ofSize: 14)
+        priceLabel.font = UIFont.systemFont(ofSize: 20)
         priceLabel.textColor = .darkGray
         addSubview(priceLabel)
         
         // 버튼 설정
-        let buttonImage = UIImage(systemName: "plus.circle.fill")
+        let buttonImage = UIImage(systemName: "plus.circle.fill",
+                                  withConfiguration: UIImage.SymbolConfiguration(pointSize: 50, weight: .bold, scale: .large))
         button.setImage(buttonImage, for: .normal)
         button.tintColor = UIColor(named: "PersonalColor")
         //button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
@@ -69,35 +73,35 @@ class CardView: UIView {
     private func setupConstraints() {
         imageView.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.6) // 이미지뷰는 top, leading, trailing가 카드뷰와 같게, 높이는 카드뷰의 0.6
+            make.height.equalToSuperview().multipliedBy(0.6) // 이미지뷰 높이: 카드뷰 높이의 60%
         }
-        
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(10)
-            make.leading.equalToSuperview().offset(10)
-            make.trailing.equalToSuperview().offset(-50)
+
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(30)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.lessThanOrEqualTo(button.snp.leading).offset(-10) // 버튼과 겹치지 않도록 설정
         }
-        
+
         priceLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(5)
-            make.leading.equalToSuperview().offset(10)
-            make.trailing.equalToSuperview().offset(-50) // 버튼 고려
+            make.top.equalTo(nameLabel.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.lessThanOrEqualTo(button.snp.leading).offset(-10) // 버튼과 겹치지 않도록 설정
             make.bottom.lessThanOrEqualToSuperview().offset(-10)
         }
-        
+
         button.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().offset(-10)
-            make.centerY.equalTo(priceLabel.snp.centerY) // 라벨과 정렬
-            make.width.height.equalTo(30) // 버튼 크기
+            make.trailing.equalToSuperview().offset(-20) // 카드뷰 오른쪽에서 20pt
+            make.bottom.equalToSuperview().offset(-10) // 카드뷰 아래에서 10pt
+            make.width.equalToSuperview().multipliedBy(0.3) // 카드뷰 너비의 30%
+            make.height.equalToSuperview().multipliedBy(0.3) // 카드뷰 높이의 30%
         }
     }
     
     // 데이터 바인딩해주는 메서드
-    private func configure(title: String, price: Int, image: UIImage?) {
-        titleLabel.text = title
+    private func configure(name: String, price: Int, image: UIImage?) {
+        nameLabel.text = name
         priceLabel.text = "\(price)원"
         imageView.image = image
     }
-    
     
 }
