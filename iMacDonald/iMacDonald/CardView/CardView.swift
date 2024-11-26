bd991404dbb7b28c64a7b47304d1e7f89e50e7b2
HwangSeokBeom <tjrqja07@naver.com>
2024-11-26 14:40:21 +0900

    ✨ feat: 카드 뷰 생성

--- iMacDonald/iMacDonald/CardView/CardView.swift
+++ iMacDonald/iMacDonald/CardView/CardView.swift
@@ -5,3 +5,20 @@
 //  Created by 유태호 on 11/25/24.
 //
 
+import UIKit
+import SnapKit
+class CardView {
+    
+    static func createCardView(title: String, price: Int, image: UIImage?) -> UIView {
+        // 카드뷰 생성
+        let cardView = UIView()
+        cardView.layer.cornerRadius = 12
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowOpacity = 0.2
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        cardView.layer.shadowRadius = 4
+        cardView.backgroundColor = .white
+     
+        return cardView
+    }
+}
