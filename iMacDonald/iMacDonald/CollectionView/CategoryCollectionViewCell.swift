04f7deae86820ad2fc8facf4052719e54a9637e9
Crois0509 <crois0509@naver.com>
2024-11-26 17:29:47 +0900

    ♻️[Refactor] 컬렉션뷰 리팩토링

--- iMacDonald/iMacDonald/CollectionView/CategoryCollectionViewCell.swift
+++ iMacDonald/iMacDonald/CollectionView/CategoryCollectionViewCell.swift
@@ -12,7 +12,7 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     // 컬렉션뷰에 추가할 때 입력할 매개변수(identifier) 정의
     static let identifier: String = "CategoryCollectionViewCell"
     
-    private let menuList: [String] = ["All", "Burger", "Chicken", "Vegan", "Side", "Drink"]
+    private let categoryList: [String] = ["All", "Burger", "Chicken", "Vegan", "Side", "Drink"]
     
     // 카테고리의 레이블 값 정의
     private let categoryLabel: UILabel = {
@@ -48,7 +48,6 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    
     /// 필수 생성 지정자
     /// - Parameter coder: 인터페이스 빌더 엔코딩 값
     ///
@@ -57,6 +56,7 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         super.init(coder: coder)
     }
     
+    /// 컬렉션 뷰의 셀을 재사용하지 못하도록 리셋
     override func prepareForReuse() {
         super.prepareForReuse()
         
@@ -68,9 +68,14 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         categoryLabel.clipsToBounds = true
         categoryLabel.layer.cornerRadius = 20
     }
-    
-    func editCategoryName(_ indexPath: Int) {
-        self.categoryLabel.text = menuList[indexPath]
+}
+
+// 카테고리 메소드
+extension CategoryCollectionViewCell {
+    /// 카테고리의 레이블 값을 세팅하는 메소드
+    /// - Parameter indexPath: 현재 셀의 index 값
+    func categoryLabelConfig(_ indexPath: Int) {
+        self.categoryLabel.text = categoryList[indexPath]
     }
     
     /// 선택된 카테고리를 강조하는 메소드
