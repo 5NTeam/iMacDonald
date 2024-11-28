50935a5fb618117a58bdb447aa7d4817d54d3ebc
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-11-28 20:01:32 +0900

    ✨ 알럿 함수 추가

--- iMacDonald/iMacDonald/ButtonView/ButtonViewAlert.swift
+++ iMacDonald/iMacDonald/ButtonView/ButtonViewAlert.swift
@@ -1,93 +1,56 @@
+//
+//  Untitled.swift
+//  iMacDonald
+//
+//  Created by YangJeongMu on 11/28/24.
+//
+
+// AlertFunction.swift
+
 import UIKit
-import SnapKit
 
-class MenuViewController: UIViewController, ButtonViewDelegate {
-    // MARK: - Properties
-    private let buttonView = ButtonView() // 변수명을 소문자로 시작하도록 수정
+/**
+ * 알럿을 표시하는 전역 함수
+ * - Parameters:
+ *   - viewController: 알럿을 표시할 뷰컨트롤러
+ *   - title: 알럿 제목
+ *   - message: 알럿 메시지
+ *   - confirmTitle: 확인 버튼 텍스트
+ *   - cancelTitle: 취소 버튼 텍스트
+ *   - confirmAction: 확인 버튼 터치 시 실행될 클로저
+ */
+func showAlert(
+    from viewController: UIViewController,
+    title: String,
+    message: String,
+    confirmTitle: String = "예",
+    cancelTitle: String = "아니오",
+    confirmAction: @escaping () -> Void
+) {
+    let alertController = UIAlertController(
+        title: title,
+        message: message,
+        preferredStyle: .alert
+    )
     
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupUI()
-        
-        // delegate 설정
-        buttonView.delegate = self
-        
-        // 테스트를 위한 예시 데이터
-        updateTotalQuantity(3)
-        updateTotalAmount(12500)
+    // 취소 액션
+    let cancelAction = UIAlertAction(
+        title: cancelTitle,
+        style: .cancel
+    )
+    
+    // 확인 액션
+    let confirmAction = UIAlertAction(
+        title: confirmTitle,
+        style: .destructive
+    ) { _ in
+        confirmAction()
     }
     
-    // MARK: - UI Setup
-    private func setupUI() {
-        view.backgroundColor = .white
-        
-        view.addSubview(buttonView)
-        
-        buttonView.snp.makeConstraints { make in
-            make.left.right.bottom.equalToSuperview()
-            make.height.equalTo(140)
-        }
-    }
+    // 액션 추가
+    alertController.addAction(cancelAction)
+    alertController.addAction(confirmAction)
     
-    // MARK: - Public Methods
-    func updateTotalQuantity(_ quantity: Int) {
-        buttonView.updateTotalQuantity(quantity)
-    }
-    
-    func updateTotalAmount(_ amount: Int) {
-        buttonView.updateTotalAmount(amount)
-    }
-    
-    // MARK: - ButtonViewDelegate Methods
-    func didTapCancelButton() {
-        let alertController = UIAlertController(
-            title: "주문 취소",
-            message: "주문을 취소하시겠습니까?",
-            preferredStyle: .alert
-        )
-        
-        let cancelAction = UIAlertAction(
-            title: "아니오",
-            style: .cancel
-        )
-        
-        let confirmAction = UIAlertAction(
-            title: "예",
-            style: .destructive
-        ) { _ in
-            // 취소 처리 로직
-            print("주문이 취소되었습니다.")
-        }
-        
-        alertController.addAction(cancelAction)
-        alertController.addAction(confirmAction)
-        present(alertController, animated: true)
-    }
-    
-    func didTapPaymentButton() {
-        let alertController = UIAlertController(
-            title: "결제 확인",
-            message: "결제를 진행하시겠습니까?",
-            preferredStyle: .alert
-        )
-        
-        let cancelAction = UIAlertAction(
-            title: "아니요",
-            style: .cancel
-        )
-        
-        let confirmAction = UIAlertAction(
-            title: "예",
-            style: .destructive
-        ) { _ in
-            // 결제 처리 로직
-            print("결제가 진행됩니다.")
-        }
-        
-        alertController.addAction(cancelAction)
-        alertController.addAction(confirmAction)
-        present(alertController, animated: true)
-    }
+    // 알럿 표시
+    viewController.present(alertController, animated: true)
 }
-
